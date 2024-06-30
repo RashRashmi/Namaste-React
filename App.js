ee9c5fa60cd0af3675ab522cd801407430377c26
Rashmitha <rashmikotian1998@gmail.com>
2024-06-30 12:23:30 +0530

    Episode -3 Laying the foundation

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "hello world from react"
-);
-
-//this object is the place where ur providing attributes to the tag
-
-console.log(heading);
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "hello world from react"
+// );
+// React.createElement => React elemement => JS Objects => html element  (render)
+// console.log(heading);
 //at the end of the React.createElement is just an object
-//It basically creates javascript object. - this is the React element  this is not the actual fiull tag.
+
+//JSX = JSX  is not  A html in js but it is a HTML  like syntax.
+
+//React Element
+const jsxHeading = <h1 id="heading">by using jsx</h1>;
+//JSX =>  React.createElement => React elemement => JS Objects => html element  (render)
+console.log(jsxHeading);
+
+const Title = function () {
+  return <h1>hey TiTLE</h1>;
+};
+
+//React component
+const HeadingComponent = () => {
+  return (
+    <div>
+        {jsxHeading}
+      <Title />
+      <h1>Writing component by using functional component</h1>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingComponent />);
 
 //this render method takes this object and convert in to heading h1 tag and put it in to the Dom.
 
